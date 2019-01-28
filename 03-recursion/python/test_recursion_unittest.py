06b8f146481fb1ff26cb6fbb708686f415a4a537
Son Dang <sondangdev@gmail.com>
2019-01-28 13:20:50 +0700

    Add python for chapter 03 - recursion

---
+++ 03-recursion/python/test_recursion_unittest.py
@@ -0,0 +1,18 @@
+import unittest
+from recursion import factorial
+
+class TestRecursion(unittest.TestCase):
+    def Setup(self):
+        pass
+
+    def test_factorial_returns_a_number(self):
+        self.assertIsInstance(factorial(3), int)
+
+    def test_factorial_returns_base_case(self):
+        self.assertEqual(factorial(1), 1)
+
+    def test_factorial_returns_correct_result(self):
+        self.assertEqual(factorial(5), 120)
+
+if __name__ == '__main__':
+    unittest.main()
