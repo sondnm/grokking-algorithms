7e533a4370f938aac6400ccfbeabc7c8c4f4f2a2
Son Dang <sondangdev@gmail.com>
2019-01-31 09:00:02 +0700

    Add python for loop sum in chapter 04 - quick sort

---
+++ 04-quick-sort/python/loop_sum.py
@@ -0,0 +1,6 @@
+def loop_sum(arr):
+    """Implementation of sum function using loop"""
+    total = 0
+    for x in arr:
+        total += x
+    return total
---
+++ 04-quick-sort/python/test_loop_sum_unittest.py
@@ -0,0 +1,20 @@
+import unittest
+from loop_sum import loop_sum
+
+class TestLoopSum(unittest.TestCase):
+    def Setup(self):
+        pass
+
+    def test_loop_sum_returns_a_number(self):
+        arr = [1, 2, 3, 4]
+        self.assertIsInstance(loop_sum(arr), int)
+
+    def test_loop_sum_returns_sum_of_all_items(self):
+        arr = [1, 2, 3, 4]
+        self.assertEqual(loop_sum(arr), 10)
+
+    def test_loop_sum_returns_zero_if_list_is_empty(self):
+        self.assertEqual(loop_sum([]), 0)
+
+if __name__ == '__main__':
+    unittest.main()
